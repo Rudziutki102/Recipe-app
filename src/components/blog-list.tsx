a1dbe17a4dbd741abfc5df2954dfd99b0d731eed
Mateusz <mateusz.makowski.102@gmail.com>
2024-05-31 20:01:14 +0200

    Skeleton of blog-list and products-list

--- src/components/blog-list.tsx
+++ src/components/blog-list.tsx
@@ -1,5 +1,13 @@
 import Separator from "./common/separator";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import FoodExample from "../assets/food_example.jpg";
 const BlogList = ({
   titleName,
   data,
@@ -10,12 +18,23 @@ const BlogList = ({
   return (
     <>
       <Separator>{titleName}</Separator>
-      <div className="flex flex-col md:grid md:grid-cols-12">
+      <div className="flex flex-col md:grid md:grid-cols-12 md:gap-5">
         {data.map((item) => (
           <div className="md:first:row-[span_3_/_auto] md:first:col-[_1_/_span_8] md:col-[_9_/_span_4]">
-            <div>{item.date}</div>
-            <div>{item.title}</div>
-            <div>{item.description}</div>
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <img src={FoodExample} alt="elo" />
+                </CardTitle>
+                <CardDescription>{item.date}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>{item.title}</p>
+              </CardContent>
+              <CardFooter>
+                <p>{item.description}</p>
+              </CardFooter>
+            </Card>
           </div>
         ))}
       </div>
--- src/components/products-list.tsx
+++ src/components/products-list.tsx
@@ -1,5 +1,13 @@
 import Separator from "./common/separator";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import FoodExample from "../assets/food_example.jpg";
 const ProductsList = ({
   titleName,
   data,
@@ -10,9 +18,24 @@ const ProductsList = ({
   return (
     <>
       <Separator>{titleName}</Separator>
-      <div className="flex flex-col md:grid md:grid-cols-12 md:gap-4">
+      <div className="flex flex-col md:grid md:grid-cols-12 md:gap-5">
         {data.map((item) => (
-          <div className="md:col-span-3">{item.name}</div>
+          <div className="md:col-span-3">
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <img src={FoodExample} alt="elo" />
+                </CardTitle>
+                <CardDescription>{item.name}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>{item.description}</p>
+              </CardContent>
+              <CardFooter>
+                <p>{item.tags}</p>
+              </CardFooter>
+            </Card>
+          </div>
         ))}
       </div>
     </>
